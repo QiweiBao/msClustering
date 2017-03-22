5e2cd77d73626e3e454a76da7ad0afbbeff4c95b
Megan <baoqiwei1993@gmail.com>
2017-03-22 19:27:29 -0400

    Remove useless annotations
    Remove useless annotations

--- data_extract.py
+++ data_extract.py
@@ -12,20 +12,6 @@ order = []
 method = []
 method_list = []
 method_list_tmp = []
-'''dir0 = "./Inputdata/05bc443e7e516b9767f6220284d75e2e2a704c1a.txt"
-dir1 = "./Inputdata/0bae9c6be8569347f743d07138d0afcb32ceebd6.txt"
-dir2 = "./Inputdata/2702de3370e40c30b4dfc3b5d81981c3564e1d2f.txt"
-dir3 = "./Inputdata/28747b348493ecb174e710f802b8ab1e1b806ca2.txt"
-dir4 = "./Inputdata/3116d69c332f3f2a8ae7020fdd997cdba81f7854.txt"
-dir5 = "./Inputdata/488ab6d9fc7a87aae254f5ae13929d73f253b6f6.txt"
-dir6 = "./Inputdata/4d9265aca864e88505fed1ffdee9ed0d22b92abd.txt"
-dir7 = "./Inputdata/567e3e3050d01c08ea02bf4865faaa6fb42bf64f.txt"
-dir8 = "./Inputdata/591b40a08e64e079d1977db6dce5d28c21f8bf7b.txt"
-dir9 = "./Inputdata/79f11c1a32806d2d3b2b48002c6423280da96f0a.txt"
-dir10 = "./Inputdata/9a0020664ed21d9488647c1ae77194a1cce493ec.txt"
-dir11 = "./Inputdata/b1bd32bf60d9f64d071f970fc90ed0b6f2c8e6ae.txt"
-dir12 = "./Inputdata/dc53d700f53c9b925fc6084ba38cb16c7a880fee.txt"
-dir13 = "./Inputdata/f4f82d64b802e98101542a270d2b41afd667a9e7.txt"'''
 
 #read data
 def fileread(dirname):
@@ -78,7 +64,6 @@ def insertzero(X, method_list):
 			zero = []
 		else:
 			index = X_tmp_arr.index(method_list[init])
-			#tmp = [X[i][index] for i in range(14)]
 			tmp = X[index]
 			res.append(tmp)
 	return res
