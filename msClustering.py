7f566e9a010194216528655c613ad6f776fa3526
Megan <baoqiwei1993@gmail.com>
2017-03-30 15:47:58 -0400

    Upload file for the project
    Add test bench for the project

---
+++ msClustering.py
@@ -0,0 +1,61 @@
+import msClustering
+import data_extract
+import MethodListExtraction
+
+def createMethodList(path):
+	#path = "/Users/qiweibao/Code/Python/InputData/processed_data_largesize/" 
+	pathwrite = path  + "MethodNameList.txt" 
+	filenames = MethodListExtraction.readfilelist(path)
+	method_list = MethodListExtraction.methodname(filenames)
+	MethodListExtraction.writeMethodList(method_list, pathwrite)
+	print "Method list created."
+
+def dataExtract(path, clu_method):
+    paths = data_extract.readfilelist(path)
+    for Tobe_Cluster_dir in paths:
+        X = data_extract.fileread(Tobe_Cluster_dir)
+        method_name_path = path + "MethodNameList.txt"
+        method_list = data_extract.method_read(method_name_path)
+        orderindex = data_extract.readmethodnames()
+        X = data_extract.insertzero(X, method_list, orderindex)
+        X = data_extract.deleteword(X)
+        print "original number of methods: " + str(len(X))
+
+        #the second parameter is threshold
+
+        X = data_extract.removeSeldomUsingMethods(X, 0.2)
+        print "number of methods after removing infrequent methods:" + str(len(X))
+        X = data_extract.reverse(X, orderindex)
+        X = msClustering.normalization(X)
+
+        if clu_method is "Hierarchical":
+        	clu_Hierarchical(X)
+        elif clu_method is "Kmeans":
+        	clu_Kmeans(X)
+        
+
+def clu_Hierarchical(X):
+        # hierarchical clustering
+        Cl_result = msClustering.HierarchicalCluster(X)
+        cophenet = msClustering.CophenetEvaluate(Cl_result, X)
+        print cophenet
+
+        dir_pieces = Tobe_Cluster_dir.split('/')
+        png_name = dir_pieces[len(dir_pieces)-1]
+        png_name = png_name[:len(png_name)-4] + ".png"
+        pic_dir = Tobe_Cluster_dir[:len(Tobe_Cluster_dir) - len(dir_pieces[len(dir_pieces)-1])] + "pics/"
+        if not os.path.exists(pic_dir):
+            os.mkdir(pic_dir)
+        print pic_dir+png_name
+        msClustering.drawHierarchical(Cl_result, pic_dir+png_name)
+
+def clu_Kmeans(X):
+        # kmeans clustering
+        msClustering.createKmeans(X)
+
+if __name__ == "__main__":
+	path = "/Users/qiweibao/Code/Python/InputData/processed_data_largesize/" 
+	createMethodList(path)
+	#choose clustering method
+	clu_method = "Hierarchical"
+	dataExtract(path, clu_method)
